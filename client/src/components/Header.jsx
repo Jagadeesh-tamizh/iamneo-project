0dd473e273e563d76f55819186e017fc4253f3bb
Jagadeesh <jagadeeshtamizh@gmail.com>
2022-10-14 14:01:48 +0530

    Uploaded react web app
    react web application is uploaded from local machine to the repository through github desktop

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className={"row"}>
+      <p className={"page-header"}>IamNeoAI Talent center</p>
+    </div>
+  );
+};
+
+export default Header;
