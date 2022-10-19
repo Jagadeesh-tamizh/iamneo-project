66465b73287d81c722607c7d336652cf9964c6e1
Jagadeesh <jagadeeshtamizh@gmail.com>
2022-10-19 22:49:11 +0530

    Drawer and appbar added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,16 @@
 import React from "react";
 import Homepage from "./pages/Homepage";
 import Header from "./components/Header";
+import MiniDrawer from "./components/MiniDrawer";
 import { DndProvider } from "react-dnd";
 import Backend from "react-dnd-html5-backend";
 
 const App = () => {
-    return (
-        <DndProvider backend={Backend}>
-            <Header />
-            <Homepage />
-        </DndProvider>
-    );
+  return (
+    <DndProvider backend={Backend}>
+      <MiniDrawer />
+    </DndProvider>
+  );
 };
 
 export default App;
